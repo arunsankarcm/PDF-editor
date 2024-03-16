1f7db68afaa0bfd162dc88d6dd691dfa81c04fed
Arun Sankar <sankarchevoor@gmail.com>
2024-03-17 01:39:49 +0530

    creatpdf and download done

--- client/src/PdfComponent.jsx
+++ client/src/PdfComponent.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Document, Page } from 'react-pdf';
+import { PDFDocument } from 'pdf-lib';
 
 function PdfComp(props) {
     const [numPages, setNumPages] = useState();
@@ -16,11 +17,39 @@ function PdfComp(props) {
 
     function handleCheckboxChange(page) {
         setCheckedPages({ ...checkedPages, [page]: !checkedPages[page] });
+    }
 
+    async function createNewPdf() {
+        
+        const selectedPages = Object.keys(checkedPages).filter(page => checkedPages[page]);
+        if (selectedPages.length === 0) {
+            alert('No pages selected. Please select at least one page.');
+            return;
+        }
+
+        const existingPdfBytes = await fetch(props.pdfFile).then(res => res.arrayBuffer());
+        const pdfDoc = await PDFDocument.load(existingPdfBytes);
+        const newPdfDoc = await PDFDocument.create();
+
+        for (const page of Object.keys(checkedPages)) {
+            if (checkedPages[page]) {
+                const [copiedPage] = await newPdfDoc.copyPages(pdfDoc, [parseInt(page) - 1]);
+                newPdfDoc.addPage(copiedPage);
+            }
+        }
+
+        const pdfBytes = await newPdfDoc.save();
+        // Create a Blob and download the PDF
+        const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = 'new-pdf-file.pdf';
+        link.click();
     }
 
     return (
         <div className='pdf-div'>
+            <button onClick={createNewPdf}>Create & Download PDF from selected pages</button>
             <Document file={props.pdfFile} onLoadSuccess={onDocumentLoadSuccess}>
                 {Array.from({ length: numPages }, (_, i) => i + 1).map((page) => (
                     <div key={page}>
@@ -44,4 +73,4 @@ function PdfComp(props) {
     );
 }
 
-export default PdfComp
+export default PdfComp;
